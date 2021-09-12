26913bd08e8b2f0b7973324a7185f44ab9fe7e99
khorh <helen.khor@googlemail.com>
2021-09-12 16:03:31 +0100

    updated tests

--- assets/tests/translator.test.js
+++ assets/tests/translator.test.js
@@ -1,37 +1,18 @@
-import { getInputLastCharacter, validateInput, sanitiseInput, convertEnglishToMorse } from "../scripts/translator.js";
-
-// Tests for the function getInputLastCharacter
-describe("Get the last character of the input value", () => {
-    // Test for a single character which is a letter
-    test("a should return a", () => {
-        expect(getInputLastCharacter("a")).toMatch("a");
-    });
-
-    // Test for a single character which is a number
-    test("1 should return 1", () => {
-        expect(getInputLastCharacter("1")).toMatch("1");
-    });
-
-    // Test for a word
-    test("ball should return l", () => {
-        expect(getInputLastCharacter("ball")).toMatch("l");
-    })
-
-    // Test for a phrase
-    test("cat in the hat should return t", () => {
-        expect(getInputLastCharacter("cat in the hat")).toMatch("t");
-    })
-});
+import { validateInput, sanitiseInput, convertEnglishToMorse } from "../scripts/translator.js";
 
 // Tests for the function validateInput
-describe("Check if the value is a-z, A-Z, 0-9 or space", () => {
-    // Test for a valid character
-    test("a should return a", () => {
-        expect(validateInput("a")).toMatch("a");
+describe("Check if the input is a-z, A-Z, 0-9 or space", () => {
+    // Test for a valid input
+    test("ABCDEFG should return true", () => {
+        expect(validateInput("ABCDEFG")).toBe(true);
     });
-    // Test for an invalid character
-    test("@ should return Invalid character", () => {
-        expect(validateInput("@")).toMatch("Invalid character");
+    // Test for an invalid input
+    test("!@£$%^& should return false", () => {
+        expect(validateInput("!@£$%^&")).toBe(false);
+    });
+    // Test for an invalid input
+    test("ABCD%^& should return false", () => {
+        expect(validateInput("ABCD%^&")).toBe(false);
     });
 });
 
@@ -45,6 +26,16 @@ describe("Convert value which is an alphabet character into lower case", () => {
     // Test for a single character which is a number
     test("1 should return 1", () => {
         expect(sanitiseInput("1")).toMatch("1");
+    });
+
+    // Test for a word
+    test("Please should return please", () => {
+        expect(sanitiseInput("Please")).toMatch("please");
+    });
+
+    // Test for a phrase
+    test("Please call NOW should return please call now", () => {
+        expect(sanitiseInput("Please call NOW")).toMatch("please call now");
     });
 });
 
